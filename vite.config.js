79d781de51072f0b9214612403cf88c464232dfd
NKRTECH <141981269+NKRTECH@users.noreply.github.com>
2024-01-31 13:47:56 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-  };
+export default defineConfig({
+  build: {
+    outDir: 'build',
+    debug: true,
+  },
+  plugins: [react()],
 });
